f066df98c08507f6c7d4b95d0bb4ac80bb66a7a0
Luwei <lw1023@msn.com>
2018-11-21 09:47:02 +0800

    动态导入(dynamic imports)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import _ from 'lodash';
+function getComponent() {
 
-function component() {
-  var element = document.createElement('div');
+  return import( /* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    var element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
+    return element;
+
+  }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,18 @@
 const path = require('path');
-const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js'
+    index: './src/index.js'
   },
   plugins: [
     new HTMLWebpackPlugin({
       title: 'Code Splitting'
     })
   ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          name: "common",
-          chunks: "initial",
-          minChunks: 2
-        }
-      }
-    }
-  },
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
